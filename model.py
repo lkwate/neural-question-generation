d9bae4f270c33d1ea2711be857425896bbb364eb
lkwate <kwatedassiloic@gmail.com>
2020-12-01 11:59:07 +0100

    update

--- model.py
+++ model.py
@@ -96,8 +96,7 @@ class Model():
     
     @staticmethod
     def _bar_custom(current, total, width = 80):
-        #progress_message = "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total)
-        sys.stdout.write("\r" + progress_message)
+        sys.stdout.write("\r" + "Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total))
         sys.flush()
         
     
